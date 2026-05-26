5c6a448e1904a92d1192df33d7b5496e3d671fd4
Alexey Moltyanskiy <alexmolt@mail.ru>
2026-05-26 21:47:43 +0300

    Update data.js (translation)

--- js/data.js
+++ js/data.js
@@ -5,12 +5,12 @@ const URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 async function loadData() {
     try {
         const response = await fetch(`${URL}/latest`);
-        if (!response.ok) throw new Error("Ошибка сети");
+        if (!response.ok) throw new Error("Error loading");
         const data = await response.json();
         return data.record; 
     } catch (error) {
-        console.error("Ошибка загрузки:", error);
-        alert("Не удалось загрузить базу библиотеки. Обновите страницу.");
+        console.error("Failed to load:", error);
+        alert("Failed to load library database. Refresh the page.");
         return [];
     }
 }
@@ -22,9 +22,9 @@ async function saveData(locations) {
     
     // Если ключа нет, спрашиваем его через всплывающее окно
     if (!masterKey) {
-        masterKey = prompt("Требуются права администратора.\nВведите Master Key:");
+        masterKey = prompt("Administrator rights are required.\nEnter Master Key:");
         if (!masterKey) {
-            alert("Сохранение отменено. Ключ не введен.");
+            alert("Save cancelled. Key not entered.");
             return; // Прерываем сохранение, если нажали Отмена
         }
     }
@@ -41,16 +41,16 @@ async function saveData(locations) {
         
         if (!response.ok) {
             if (response.status === 401 || response.status === 403) {
-                alert("Ошибка доступа: Неверный Master Key!");
+                alert("Access error: Invalid Master Key!");
                 localStorage.removeItem("admin_key"); // Удаляем неправильный ключ
             }
-            throw new Error("Ошибка сохранения");
+            throw new Error("Saving error");
         }
         
         // Если всё успешно, запоминаем ключ в браузере, чтобы не вводить каждый раз
         localStorage.setItem("admin_key", masterKey);
         
     } catch (error) {
-        console.error("Ошибка сохранения:", error);
+        console.error("Saving error:", error);
     }
 }
